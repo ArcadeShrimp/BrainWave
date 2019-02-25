77139add29b07941340fe2c379910be661e0ce2f
billyrrr <billrao@me.com>
2019-02-24 16:46:13 -0800

    Add command object

---
+++ command.py
@@ -0,0 +1,27 @@
+class Tracker:
+    """
+    Tracks PsychoPy Calibration and MuseLsL Data
+    """
+
+    def __init__(self, inlet):
+        """
+        Initialize with inlet.
+        :param inlet:
+        """
+        pass
+
+    def _start_recording(self, arr):
+        """ Write smooth band power to the df
+
+        :return:
+        """
+        pass
+
+    def start_stage(self, mode=None, stage=0):
+        """
+            Start relax stage and record data.
+
+        :return:
+        """
+        pass
+
